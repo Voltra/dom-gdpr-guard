95b32b52da3a465f389c815680472062507b0f56
Voltra <ludwig.guerin.98@gmx.fr>
2020-07-14 16:22:03 +0200

    -> v2.0.0

--- lib/src/render.d.ts
+++ lib/src/render.d.ts
@@ -1,4 +1,4 @@
-import { GdprSavior, GdprManagerFactory } from "gdpr-guard";
+import { GdprManager, GdprSavior, GdprManagerFactory } from "gdpr-guard";
 import { Rendered, GroupRenderFunction, ManagerRenderFunction, GuardRenderFunction } from "./Renderer";
 /**
  * Configuration for the render function
@@ -17,13 +17,35 @@ export interface RenderPayload {
      */
     renderGuard: GuardRenderFunction;
 }
+/**
+ * The payload required for the Savior API
+ */
 export interface GdprPayload {
+    /**
+     * The savior to use
+     */
     savior: GdprSavior;
+    /**
+     * A factory to a {@link GdprManager}
+     */
     managerFactory: GdprManagerFactory;
+}
+/**
+ * Result of the render function
+ */
+export interface GdprRenderResult {
+    /**
+     * The rendered element
+     */
+    rendered: Rendered;
+    /**
+     * The manager used to render
+     */
+    manager: GdprManager;
 }
 /**
  * Render the current manager state (you will manually handle re-renders)
  * @param manager - The manager state to use for rendering
  * @param payload - The render configuration
  */
-export declare const render: (gdpr: GdprPayload, payload: RenderPayload) => Promise<Rendered>;
+export declare const render: (gdpr: GdprPayload, payload: RenderPayload) => Promise<GdprRenderResult>;
